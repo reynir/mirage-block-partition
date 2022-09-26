e227d16d8c67ac3468cc2f3caadf6c01accc3575
Reynir Björnsson <reynir@reynir.dk>
2022-09-26 16:47:42 +0200

    Use exceptions in connect

--- lib/mirage_block_partition_mbr.mli
+++ lib/mirage_block_partition_mbr.mli
@@ -1,6 +1,4 @@
 module Make(B : Mirage_block.S) : sig
   include Mirage_block.S
-  type connect_error = private [> `Bad_partition | `Overlapping_partitions | `Sector_size of int ]
-  val pp_connect_error : connect_error Fmt.t
-  val connect : B.t -> ((Mbr.Partition.t * t) list, connect_error) result Lwt.t
+  val connect : B.t -> ((Mbr.Partition.t * t) list, [ `Overlapping_partitions ]) result Lwt.t
 end
