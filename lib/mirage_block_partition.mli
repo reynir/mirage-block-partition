da9b1af9395fca89e7898cabda7c2ae5c9a04a00
Reynir Björnsson <reynir@reynir.dk>
2022-09-27 16:14:23 +0200

    Update documentation
    - Document raised exceptions
    - Update README.md:
      - Update the example to the changed interface
      - Explain more
      - mirage-block-partition-mbr

--- lib/mirage_block_partition.mli
+++ lib/mirage_block_partition.mli
@@ -5,11 +5,13 @@ module Make(B : Mirage_block.S) : sig
   (** [connect first_sectors b] returns two block device partitions of b with
       the first partition of [first_sectors] sectors, and the second partition
       the remaining space, if any.
+      @raises Invalid_argument if the partition point is outside the device.
   *)
 
   val subpartition : int64 -> t -> (t * t)
   (** [subpartition first_sectors b] further partitions a partition into two sub
       partitions.
+      @raises Invalid_argument if the partition point is outside the partition.
   *)
 
   val get_offset : t -> int64
